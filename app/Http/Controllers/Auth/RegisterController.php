45cf181931d90eaef836cc66ef017cea6edac457
delliCC <desarrollo.web.chontalpa@gmail.com>
2022-08-02 09:49:33 -0500

    creando login

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -70,17 +70,4 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
         ]);
     }
-
-    // Register
-    public function showRegistrationForm()
-    {
-        $pageConfigs = [
-            'bodyClass' => "bg-full-screen-image",
-            'blankPage' => true
-        ];
-
-        return view('/auth/register', [
-            'pageConfigs' => $pageConfigs
-        ]);
-    }
 }
